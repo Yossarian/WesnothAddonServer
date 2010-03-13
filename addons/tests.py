e1f0b59d3ebebaa81637cd60cdd5112bfbafb901
bigmac <bigmac@.(none)>
2010-03-13 16:38:04 +0100

    First unit test, fixture
    Signed-off-by: bigmac <>

--- addons/tests.py
+++ addons/tests.py
@@ -6,18 +6,15 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from addons.models import *
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+	fixtures = ['testdb.json']
+	#Caution: assuming certain specific content of testdb.json.
+	#Any change to the test database must be explicitly reflected
+	#in test code unless the test doesn't reference the database.
+	#Changes the test code makes to the database are not persistent.
+	def test_getRating(self):
+		a = Addon.objects.get(id=3)
+		self.assertEquals(a.get_rating(), 3.0)
+	
