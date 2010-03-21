97af1539a29a12d43e538efbe8da71235ac87e6e
unknown <Yossarian@.(none)>
2010-03-21 18:39:18 +0100

    Added logger (fixes issue #18)

--- addons/views.py
+++ addons/views.py
@@ -4,6 +4,18 @@ from addons.models import *
 import re
 from django.utils.datetime_safe import datetime
 
+import logging   
+import logging.handlers
+logger = logging.getLogger('project_logger')
+logger.setLevel(logging.INFO)
+
+LOG_FILENAME = 'log.txt'
+LOG_MSG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when = 'midnight')
+formatter = logging.Formatter(LOG_MSG_FORMAT)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 def index(request):
 	addon_list = Addon.objects.all().order_by('-name')
 	for addon in addon_list:
@@ -22,6 +34,7 @@ def details(request, addon_id):
 	return render_to_response('addons/details.html', {'addon': addon})
 
 def getFile(request, addon_id):
+	logger.info("Download of addon "+addon_id+" requested from "+request.META['REMOTE_ADDR']);
 	addon = Addon.objects.get(id=addon_id)
 	addon.downloads = addon.downloads + 1
 	addon.save()
@@ -84,13 +97,16 @@ def publish(request):
 		except:
 			errors_zip = True
 		if errors_zip or errors_pbl:
+			logger.info("Attempt to publish an addon by "+login+" from "+request.META['REMOTE_ADDR']+" failed due to invalid files.");
 			return render_to_response('addons/publishForm.html', {'errors_zip' : errors_zip,
 			'errors_pbl' : errors_pbl,
 			'loginVal' : login})
 		else:
+			logger.info("User "+login+" from "+request.META['REMOTE_ADDR']+" has successfully published addon #"+addon.id+" ("+addon.name+")");
 			return render_to_response('addons/publishForm.html', {'publish_success' : True,
 			'loginVal' : login})
 	else:
+		logger.info("Attempt to login as "+login+" from "+request.META['REMOTE_ADDR']+" failed during an attempt to publish.");
 		return render_to_response('addons/publishForm.html', {'errors_credentials' : True,
 		'loginVal' : login})
 	
@@ -110,7 +126,11 @@ def remove(request, addon_id):
 	
 	if not (errors_permissions and errors_credentials):
 		addon.delete()
-	
+		logger.info("Addon #"+addon_id+"("+addon.name+") deleted by user "+login)
+	if (errors_credentials)
+		logger.info("Attempt to login as "+login+" from "+request.META['REMOTE_ADDR']+" failed during an attempt to remove addon #"+addon_id+"("+addon.name+")");
+	if (errors_credentials)
+		logger.info("Attempt to remove addon #"+addon_id+"("+addon.name+") by "+login+" from "+request.META['REMOTE_ADDR']+" failed due to insufficient credentials.");
 	return render_to_response('addons/confirmRemove.html',
 							  {'addon_id':addon_id,
 							   'addon': addon, 'errors_credentials':errors_credentials,
@@ -120,12 +140,16 @@ def remove(request, addon_id):
 	
 def adminWescampLog(request):
 	if request.user.is_staff:
+		logger.info("Foobar admin reads some Wescamp logs");
 		return HttpResponse("<stub> Stardate 8130: 18:30 MyAddon1, MyAddon5 sent to Wescamp")
 	else:
+		logger.info("Foobar noon-admin attempts to read some Wescamp logs");
 		return HttpResponse("<stub> You are not an admin!")
 	
 def adminWescampUpdate(request):
 	if request.user.is_staff:
+		logger.info("Foobar admin updates some stuff@Wescamp");
 		return HttpResponse("<stub> Addons that changed: MyAddon1, MyAddon5 - sent to Wescamp")
 	else:
+		logger.info("Foobar non-admin attempts to update some stuff@Wescamp");
 		return HttpResponse("<stub> You are not an admin!")
