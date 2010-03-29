cae6c28d112156888b9746d0446dd9d5342f188d
boombel <rramocki@gmail.com>
2010-03-30 01:06:32 +0200

    basic phpauth completed
    - db constants needs to be moved
    - need more (better) error checking

--- addons/phpbb_auth.py
+++ addons/phpbb_auth.py
@@ -1,14 +1,25 @@
 from django.conf import settings
 from django.contrib.auth.models import User, check_password
+from phpbb_hash import check_pswd
+import MySQLdb
+
+DB_HOST="127.0.0.1"
+DB_NAME="phpbb"
+DB_USER="root"
+DB_PASSWORD="root"
+DB_USER_TABLE="phpbb_users"
 
 class PhpbbBackend:
 	def authenticate(self, username=None, password=None):
-		login_valid = (username == 'master')
-		pwd_valid = (password == 'master')
-		if login_valid and pwd_valid:
+		conn = MySQLdb.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+		c = conn.cursor()
+		c.execute("SELECT user_password FROM "+DB_USER_TABLE+
+			  " WHERE UPPER(username)=UPPER('"+username+"')")
+		if c.rowcount == 0:
+			return None
+		if check_pswd(password, c.fetchall()[0][0]):
 			try:
 				user = User.objects.get(username=username)
-				user.is_staff = True
 			except User.DoesNotExist:
 					user = User(username=username, password='whatever')
 					user.save()
