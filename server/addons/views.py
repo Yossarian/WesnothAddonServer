4791361fbf1389bef72fbc426057b38e72783e87
boombel <rramocki@gmail.com>
2010-04-20 00:59:52 +0200

    publish changes
    - can verify pbl
    - can publish just wml
    - can publish wml by post

--- server/addons/views.py
+++ server/addons/views.py
@@ -4,6 +4,7 @@ from addons.models import *
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.datetime_safe import datetime
 from django.contrib.auth import authenticate
+from django.core.files import File
 from django.http import Http404
 from settings import MEDIA_ROOT
 import logging   
@@ -128,116 +129,168 @@ def rate(request, addon_id):
 	else:
 		return render_to_response('addons/details.html', {'rated' : True, 'addon_id': addon_id, 'addon': addon, 'rate_val': value})
 
+def check_pbl(pbl_data):
+	keys_vals = {}
+	num = 0
+	for l in pbl_data:
+		num += 1
+		m = re.match(r"^(.*)=\"(.*)\"$", l)
+		if m == None:
+			raise Exception('Line '+str(num)+' is invalid')
+		keys_vals[m.group(1)] = m.group(2)
+		
+	needed_keys = ['title', 'icon', 'version', 'description', 'author', 'type']
+
+	for key in needed_keys:
+		try:
+			keys_vals[key]
+		except LookupError:
+			raise Exception('Pbl doesn\'t have ' + key + ' key')
+	return keys_vals
 
 def publish(request):
 	login = username=request.POST['login']
 	user = authenticate(username=login, password=request.POST['password'])
-	
-	if user is not None:
-		errors_pbl = False
-		errors_zip = False
+
+	if user is None:	
+		logger.info(	"Attempt to login as " + login +
+				" from " + request.META['REMOTE_ADDR'] +
+				" failed during an attempt to publish.")
+
+		return render_to_response('addons/publishForm.html', {'errors_credentials' : True,
+									'loginVal' : login})
+
+	errors_pbl = False
+	errors_wml = False
+	file_wml = None
+	file_pbl = None
+	try:
+		file_pbl = request.FILES['pbl']
+	except:
+		errors_pbl = True
+	try:
+		file_wml = request.FILES['wml']
+	except:
+		errors_wml = True
+
+	if errors_wml and errors_pbl:
+		logger.info(	"Attempt to publish an addon by " + login +
+				" from " + request.META['REMOTE_ADDR'] + 
+				" failed due to invalid files.")
+		return render_to_response('addons/publishForm.html', {'errors_wml' : errors_wml,
+								      'errors_pbl' : errors_pbl,
+							              'loginVal' : login})
+
+	def error_response(title, error):
+		return render_to_response('addons/error.html',
+					  {'errorType':title, 'errorDesc':error})
+
+	keys_vals = {}
+	if file_pbl != None:
 		try:
-			pbl = request.FILES['pbl']
-		except:
-			errors_pbl = True
-		try:
-			file = request.FILES['zip']
-		except:
-			errors_zip = True
-		if errors_zip or errors_pbl:
-			logger.info("Attempt to publish an addon by "+login+" from "+request.META['REMOTE_ADDR']+" failed due to invalid files.");
-			return render_to_response('addons/publishForm.html', {'errors_zip' : errors_zip,
-			'errors_pbl' : errors_pbl,
-			'loginVal' : login})
-
-		def error_response(title, error):
-			return render_to_response('addons/error.html',
-						  {'errorType':title, 'errorDesc':error})
-
-		keys_vals = {}
-		num = 0
-		for l in pbl.readlines():
-			num += 1
-			m = re.match(r"^(.*)=\"(.*)\"$", l)
-			if m == None:
-				return error_response('Pbl error', ['Line '+str(num)+' is invalid'])
-			keys_vals[m.group(1)] = m.group(2)
-		
-		needed_keys = ['title', 'icon', 'version', 'description', 'author', 'type']
-
-		for key in needed_keys:
-			try:
-				keys_vals[key]
-			except LookupError:
-				return error_response('PBL error', ['Pbl doesn\'t have ' + key + ' key'])
+			keys_vals = check_pbl(file_pbl.readlines())
+		except Exception as inst:
+			return error_response('Pbl error', [inst.args[0]])
 
 		try:
 			addon_type = AddonType.objects.get(type_name=keys_vals['type'])
 		except ObjectDoesNotExist:
 			return error_response('PBL error', ['Addon has a wrong type'])
 
-		authors_str = []
-		for author in re.split(r",", keys_vals['author']):
-			authors_str.append(author)
-
 		try:
 			addon = Addon.objects.get(name=keys_vals['title'])
 			if len(addon.authors.filter(name=login)) == 0:
 				return error_response('Author error', ['This user is not one of authors'])
-			addon.uploads += 1
-			if addon.file_tbz:
-				addon.file_tbz.delete()
-			if addon.file_wml:
-				addon.file_wml.delete()
 		except ObjectDoesNotExist:
-			addon = Addon()
-			addon.name = keys_vals['title']
-			addon.uploads = 1
-			addon.downloads = 0
+			pass
 
-		file.name = addon.name + '.wml'		
+	if file_pbl and ('wml' not in request.POST or request.POST['wml'] == '') and file_wml == None:
+		raise Exception("PBL VERIFICATION FINISHED SUCCESSFUL")
 
-		tmp_dir_name = "%016x" % random.getrandbits(128)
-		cs = CampaignClient()
-				
-		file_data = file.read()
-		decoded_wml = cs.decode(file_data)
-
-		cs.unpackdir(decoded_wml, tmp_dir_name, verbose = False)
-		tarname = os.path.join(MEDIA_ROOT, "addons/") + addon.name + ".tar.bz2"
-		Popen(["tar", "cjf", tarname, "-C", tmp_dir_name, '.']).wait()
-		shutil.rmtree(tmp_dir_name, True)
-		addon.file_tbz = "addons/" + addon.name + ".tar.bz2"
-
-		addon.ver = keys_vals['version']
-		addon.img = keys_vals['icon']
-		addon.desc = keys_vals['description']
-		addon.type = addon_type
-		addon.file_wml = file
-		addon.lastUpdate = datetime.now()
-		addon.save()
-
-		addon.authors.clear()
-
-		for a in authors_str:
-			author = None
-			try:
-				author = Author.objects.get(name=a)
-			except ObjectDoesNotExist:
-				author = Author(name=a)
-				author.save()
-			addon.authors.add(author)
-		
-		addon.save()
-
-		logger.info("User "+login+" from "+request.META['REMOTE_ADDR']+" has successfully published addon #"+str(addon.id)+" ("+addon.name+")");
-		return render_to_response('addons/publishForm.html', {'publish_success' : True,
-								      'loginVal' : login})
+	cs = CampaignClient()
+	if file_wml != None:
+		file_data = file_wml.read()
 	else:
-		logger.info("Attempt to login as "+login+" from "+request.META['REMOTE_ADDR']+" failed during an attempt to publish.");
-		return render_to_response('addons/publishForm.html', {'errors_credentials' : True,
-		'loginVal' : login})
+		if 'wml' not in request.POST:
+			raise Exception("NO WML FILE DATA")
+		file_data = request.POST['wml']
 	
+	decoded_wml = cs.decode(file_data)
+
+  	for field in ["title", "author", "description", "version", "icon", "type"]:
+		keys_vals[field] = decoded_wml.get_text_val(field)
+		if keys_vals[field] == None:
+			raise Exception("WML key error (PBL IN WML)")
+
+	try:
+		addon_type = AddonType.objects.get(type_name=keys_vals['type'])
+	except ObjectDoesNotExist:
+		return error_response('WML PBL error', ['Addon has a wrong type'])
+
+	try:
+		addon = Addon.objects.get(name=keys_vals['title'])
+		if len(addon.authors.filter(name=login)) == 0:
+			return error_response('Author error', ['This user is not one of authors'])
+		addon.uploads += 1
+		if addon.file_tbz:
+			addon.file_tbz.delete()
+		if addon.file_wml:
+			addon.file_wml.delete()
+	except ObjectDoesNotExist:
+		addon = Addon()
+		addon.name = keys_vals['title']
+		addon.uploads = 1
+		addon.downloads = 0
+
+	
+
+	if file_wml != None:
+		file_wml.name = addon.name + '.wml'
+	else:
+		file = open(os.path.join(MEDIA_ROOT, "addons/") + addon.name + ".wml", 'w')
+		file.write(file_data)
+		file.close()
+		file_wml =  "addons/" + addon.name + ".wml"
+		
+
+	tmp_dir_name = "%016x" % random.getrandbits(128)
+	cs.unpackdir(decoded_wml, tmp_dir_name, verbose = False)
+	tarname = os.path.join(MEDIA_ROOT, "addons/") + addon.name + ".tar.bz2"
+	Popen(["tar", "cjf", tarname, "-C", tmp_dir_name, '.']).wait()
+	shutil.rmtree(tmp_dir_name, True)
+	addon.file_tbz = "addons/" + addon.name + ".tar.bz2"
+
+	addon.ver = keys_vals['version']
+	addon.img = keys_vals['icon']
+	addon.desc = keys_vals['description']
+	addon.type = addon_type
+	addon.file_wml = file_wml
+	addon.lastUpdate = datetime.now()
+	addon.save()
+
+	addon.authors.clear()
+
+	authors_str = []
+	for author in re.split(r",", keys_vals['author']):
+		authors_str.append(author)
+
+	for a in authors_str:
+		author = None
+		try:
+			author = Author.objects.get(name=a)
+		except ObjectDoesNotExist:
+			author = Author(name=a)
+			author.save()
+		addon.authors.add(author)
+		
+	addon.save()
+
+	logger.info(	"User " + login + " from " + request.META['REMOTE_ADDR'] + 
+					" has successfully published addon #" + str(addon.id) +
+					" ("+addon.name+ ")")
+	return render_to_response('addons/publishForm.html', {'publish_success' : True,
+								      'loginVal' : login})
+			
 def publishForm(request):
 	return render_to_response('addons/publishForm.html')
 	
