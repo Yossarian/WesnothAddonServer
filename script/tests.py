15b7bcef38d5cf41a51c6f509773174f1e334522
Yossarian <157482@student.pwr.wroc.pl>
2010-05-10 21:35:11 +0200

    Added further script tests (issue 38)

--- script/tests.py
+++ script/tests.py
@@ -1,4 +1,4 @@
-import os, subprocess, unittest, codecs, time
+import os, subprocess, unittest, codecs, time, filecmp
 #test.db fixture to be loaded manually.
 
 class TestClass(unittest.TestCase):
@@ -35,9 +35,14 @@ class TestClass(unittest.TestCase):
 
 	def test_existing_publish(self):
 		somefile = file("publish.txt","w")
-		args = ["wesnoth_addon_manager.py", "-u", "../test_data/game_publish_test/BraveWings","-L", "autor1", "-P", "autor1"]
+		args = ["wesnoth_addon_manager.py", "-u", "../test_data/game_publish_test/Brave Wings","-L", "autor1", "-P", "autor1"]
 		proc = subprocess.Popen(args, shell = True, stdout = somefile, stderr = somefile)
-		self.assertTrue(True)
+		while proc.poll() == None:
+			time.sleep(0.001)
+		out = ''
+		for line in codecs.open("publish.txt"):
+			out = out + line
+		self.assertTrue('Addon published successfully' in out)
 
 	def test_nonexisting_publish(self):
 		somefile = file("publish_null.txt","w")
@@ -50,12 +55,32 @@ class TestClass(unittest.TestCase):
 			break
 		self.assertTrue('Cannot open file fooooooobar' in out)
 		
-	#def test_downloaded_content(self):
-		#somefile = file("down.txt","w")
-		#proc = subprocess.Popen("wesnoth_addon_manager.py -Rd \"BraveWings*\"", shell = True, stdout = somefile)
-		#while proc.poll() == None:
-			#time.sleep(0.001)
-		#self.assertTrue(os.path.exists("BraveWings"))
+	def test_downloaded_content(self):
+		somefile = file("down.txt","w")
+		proc = subprocess.Popen("wesnoth_addon_manager.py -d \"Brave Wings*\"", shell = True, stdout = somefile)
+		while proc.poll() == None:
+			time.sleep(0.001)
+		dircomp = filecmp.dircmp("Brave Wings","../test_data/game_publish_test/Brave Wings", ignore = ['_server.pbl','_info.cfg'])
+		self.assertTrue(dircomp.left_list==dircomp.right_list)
+		
+	def test_remove_published(self):
+		somefile = file("remove.txt","w")
+		args = ["wesnoth_addon_manager.py", "-u", "../test_data/game_publish_test/Brave Wings","-L", "autor1", "-P", "autor1"]
+		proc = subprocess.Popen(args, shell = True, stdout = somefile)
+		while proc.poll() == None:
+			time.sleep(0.001)
+		args = ["wesnoth_addon_manager.py", "-r", "Brave Wings","-L", "autor1", "-P", "autor1"]
+		proc = subprocess.Popen(args, shell = True, stdout = somefile)
+		while proc.poll() == None:
+			time.sleep(0.001)
+		proc = subprocess.Popen("wesnoth_addon_manager.py -d \"Brave Wings*\"", shell = True, stdout = somefile)
+		while proc.poll() == None:
+			time.sleep(0.001)
+		out = ''
+		for line in codecs.open("down_null.txt"):
+			out = line
+			break
+		self.assertTrue('No addon found' in out)
 		
 if __name__ == '__main__':
     unittest.main()
